4aae74722d1b76fc194c83c3a3350766ab8c10db
anandheraksaxena <anandheraksaxena@gmail.com>
2018-08-19 22:08:04 +0530

    inster

---
+++ AHS_NY_Demo-App1/src/main/java/com/AHS_NY_MavenSampleProj1/app/App.java
@@ -0,0 +1,13 @@
+package com.AHS_NY_MavenSampleProj1.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
